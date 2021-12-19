7c87ef4bef49b0664ece3db776f94407df054f0d
Jakub Wyłucki <01159628@pw.edu.pl>
2021-12-19 22:49:24 +0100

    LCS

--- aisd_lab_9_longest_common_subsequence/src/main/java/pl/edu/pw/ee/LongestCommonSubsequence.java
+++ aisd_lab_9_longest_common_subsequence/src/main/java/pl/edu/pw/ee/LongestCommonSubsequence.java
@@ -2,32 +2,32 @@ package pl.edu.pw.ee;
 
 class LongestCommonSubsequence {
 
-    private String firstStr;
-    private String secondStr;
     private Element[][] tab;
-    private int firstLen;
-    private int secondLen;
-    
-    public LongestCommonSubsequence(String firstStr, String secondStr){
-        validateString(firstStr);
-        validateString(secondStr);
-        this.firstStr = firstStr;
-        this.secondStr = secondStr;
-        firstLen = firstStr.length() + 1;
-        secondLen = secondStr.length() + 1;
-        tab = new Element[firstLen][secondLen];
-        
-        for(int i = 0; i < firstLen; i++){
-            for(int j = 0; j < secondLen; j++){
-                if(i == 0){
+    private String[][] strTab;
+    private String topStr;
+    private String leftStr;
+    private int topLen;
+    private int leftLen;
+
+    public LongestCommonSubsequence(String topStr, String leftStr) {
+        validateString(topStr);
+        validateString(leftStr);
+        this.topStr = topStr;
+        this.leftStr = leftStr;
+        this.topLen = topStr.length() + 1;
+        this.leftLen = leftStr.length() + 1;
+        tab = new Element[leftLen][topLen];
+        for (int i = 0; i < leftLen; i++) {
+            for (int j = 0; j < topLen; j++) {
+                if (i == 0) {
                     tab[i][j] = new Element(0, -1);
-                } else if(j == 0){
+                } else if (j == 0) {
                     tab[i][j] = new Element(0, -1);
                 } else {
-                    if(firstStr.charAt(i - 1) == secondStr.charAt(j - 1)){
+                    if (leftStr.charAt(i - 1) == topStr.charAt(j - 1)) {
                         tab[i][j] = new Element(tab[i - 1][j - 1].getValue() + 1, 2);
-                    } else if(tab[i - 1][j].getValue() >= tab[i][j - 1].getValue()){
-                        tab[i][j] = new Element(tab[i -1][j].getValue(), 0);
+                    } else if (tab[i - 1][j].getValue() >= tab[i][j - 1].getValue()) {
+                        tab[i][j] = new Element(tab[i - 1][j].getValue(), 0);
                     } else {
                         tab[i][j] = new Element(tab[i][j - 1].getValue(), 1);
                     }
@@ -36,62 +36,153 @@ class LongestCommonSubsequence {
         }
     }
 
-    public String findLCS(){
+    public String findLCS() {
         String anser = "";
-        int i = firstLen - 1;
-        int j = secondLen - 1;
+        int i = leftLen - 1;
+        int j = topLen - 1;
         int x = tab[i][j].getValue();
-        while(x > 0){
-            if(firstStr.charAt(i - 1) == secondStr.charAt(j - 1)){
-                anser = firstStr.charAt(i - 1) + anser;
-            }
-            if(tab[i][j].getDirection() == 2){
+        while (x > 0) {
+            if (tab[i][j].getDirection() == 2) {
+                anser = leftStr.charAt(i - 1) + anser;
                 i--;
                 j--;
                 x = tab[i][j].getValue();
-            } else if(tab[i][j].getDirection() == 1){
+            } else if (tab[i][j].getDirection() == 1) {
                 j--;
                 x = tab[i][j].getValue();
-            } else if(tab[i][j].getDirection() == 0){
+            } else if (tab[i][j].getDirection() == 0) {
                 i--;
                 x = tab[i][j].getValue();
             }
         }
-	    return anser;
+        return anser;
     }
 
-    //public void display(){
-    public String[][] display(){
-        int i = firstLen * 2 + 2;
-        int j = secondLen * 2 + 2;
-        String [][] strTab = new String[i][j];
-        for (int y = 0; y < i; y++){
-            for(int x = 0; x < j; x++){
-                if (y % 2 == 0){
-                    if( x % 2 == 0){
-                        strTab[y][x] = " ";
-                    } else {
-                        strTab[y][x] = "|";
-                    }
-                } else if (y % 2 == 1){
-                    if( x % 2 == 0){
-                        strTab[y][x] = " ";
-                    } else {
+    public void display() {
+        int i = (leftLen + 1) * 4 + 1;
+        int j = (topLen) * 6 + 9;
+        strTab = new String[i][j];
+
+        for (int y = 0; y < i; y++) {
+            for (int x = 0; x < j; x++) {
+                if (y % 4 == 0) {
+                    if (x == 0 || ((x - 2) % 6 == 0 && x != 2)) {
                         strTab[y][x] = "+";
+                    } else {
+                        strTab[y][x] = "-";
+                    }
+                } else {
+                    if ((x + 1) % 6 == 0) {
+                        if (y == 2 && x > 16) {
+                            addString(y, x);
+                        } else if (x > 10 && (y - 2) % 4 == 0) {
+                            strTab[y][x] = "0";
+                        }
+                    } else if ((y - 2) % 4 == 0 && y > 9) {
+                        if (x == 4) {
+                            addString(y, x);
+                        } else if ((x + 1) % 6 == 0) {
+                            strTab[y][x] = "0";
+                        }
+                    } else {
+                    }
+                    if (x == 0 || ((x - 2) % 6 == 0 && x != 2)) {
+                        strTab[y][x] = "|";
+                    } else {
+                        if (strTab[y][x] == null) {
+                            strTab[y][x] = " ";
+                        }
                     }
                 }
             }
         }
-        return strTab;
+        strTab[2][11] = " ";
+
+        for (int y = 0; y < leftLen - 1; y++) {
+            for (int x = 0; x < topLen - 1; x++) {
+                strTab[10 + y * 4][17 + x * 6] = String.valueOf(tab[y + 1][x + 1].value);
+            }
+        }
+
+        int a = leftLen - 1;
+        int b = topLen - 1;
+        int val = tab[a][b].getValue();
+        while (val > 0) {
+            if (tab[a][b].getDirection() == 2) {
+                strTab[6 + a * 4 - 1][11 + b * 6 - 2] = "\\";
+                a--;
+                b--;
+                val = tab[a][b].getValue();
+            } else if (tab[a][b].getDirection() == 1) {
+                strTab[6 + a * 4][11 + b * 6 - 2] = "<";
+                b--;
+                val = tab[a][b].getValue();
+            } else if (tab[a][b].getDirection() == 0) {
+                strTab[6 + a * 4 - 1][11 + b * 6] = "^";
+                a--;
+                val = tab[a][b].getValue();
+            }
+        }
+
+        String ans = "";
+        for (int y = 0; y < i; y++) {
+            for (int x = 0; x < j; x++) {
+                ans += strTab[y][x];
+            }
+            ans += "\n";
+        }
+        System.out.print(ans);
     }
 
-    public class Element{
+    private void addString(int y, int x) {
+        String a;
+        if (x > 16) {
+            a = topStr.substring((x - 17) / 6, (x - 17) / 6 + 1);
+        } else {
+            a = leftStr.substring((y - 10) / 4, (y - 10) / 4 + 1);
+        }
+        a = checkString(a);
+
+        if (a.length() == 1) {
+            strTab[y][x] = a;
+        } else if (a.length() == 2) {
+            strTab[y][x - 1] = a.substring(0, 1);
+            strTab[y][x] = a.substring(1, 2);
+        } else if (a.length() == 3) {
+            strTab[y][x - 2] = a.substring(0, 1);
+            strTab[y][x - 1] = a.substring(1, 2);
+            strTab[y][x] = a.substring(0, 3);
+        }
+    }
+
+    private String checkString(String a) {
+        int t = a.charAt(0);
+        if (t == 9) {
+            return "\\t";
+        } else if (t == 8) {
+            return "\\b";
+        } else if (t == 10) {
+            return "\\n";
+        } else if (t == 13) {
+            return "\\r";
+        } else if (t == 12) {
+            return "\\f";
+        } else if (t == 39) {
+            return "\\'";
+        } else if (t == 34) {
+            return "\\\"";
+        } else if (t == 92) {
+            return "\\\\";
+        } else {
+            return a;
+        }
+    }
+
+    private class Element {
         private int value;
         private int direction;
-        // 0 - góra
-        // 1 - lewo
-        // 2 - lewy górny
-        Element(int value, int direction){
+
+        Element(int value, int direction) {
             this.value = value;
             this.direction = direction;
         }
@@ -105,32 +196,9 @@ class LongestCommonSubsequence {
         }
     }
 
-    private void validateString(String a){
-        if(a == null || a == ""){
-            throw new IllegalArgumentException("String cannot be null or empty");
+    private void validateString(String a) {
+        if (a == null) {
+            throw new IllegalArgumentException("String cannot be null");
         }
     }
-
-
-    //do testowania
-    public int getFirstLen() {
-        return firstLen;
-    }
-
-    public int getSecondLen() {
-        return secondLen;
-    }
-
-    public Element[][] getTab() {
-        return tab;
-    }
-
-    public String getFirstStr() {
-        return firstStr;
-    }
-
-    public String getSecondStr() {
-        return secondStr;
-    }
-
 }
--- aisd_lab_9_longest_common_subsequence/src/test/java/pl/edu/pw/ee/LongestCommonSubsequenceTest.java
+++ aisd_lab_9_longest_common_subsequence/src/test/java/pl/edu/pw/ee/LongestCommonSubsequenceTest.java
@@ -1,19 +1,220 @@
 package pl.edu.pw.ee;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-public class LongestCommonSubsequenceTest 
-{
+public class LongestCommonSubsequenceTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldReturnExceeptionWhenStringInNull(){
+        LongestCommonSubsequence t = new LongestCommonSubsequence(null, "tar");
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldReturnExceeptionWhenStringInNullSecond(){
+        LongestCommonSubsequence t = new LongestCommonSubsequence("tar", null);
+    }
+
     @Test
-    public void test(){
-        LongestCommonSubsequence t = new LongestCommonSubsequence("pilowalem", "pilem");
-        System.out.println(t.findLCS());
-        String[][] tab = t.display();
-        for(int i = 0; i < tab.length; i++){
-            for(int j = 0; j < tab[i].length; j++){
-                System.out.print(tab[i][j]);
-            }
-            System.out.println();
-        }
+    public void shouldReturnTrueToFindLongestLCS_NoLCS(){
+        LongestCommonSubsequence t = new LongestCommonSubsequence("abcd", "");
+        String expected = "";
+        String actual = t.findLCS();
+        assertEquals(expected, actual); 
+    }
+
+    @Test
+    public void shouldReturnTrueToFindLongestLCS_StringAreEmpty(){
+        LongestCommonSubsequence t = new LongestCommonSubsequence("", "");
+        String expected = "";
+        String actual = t.findLCS();
+        assertEquals(expected, actual); 
+    }
+
+    @Test
+    public void shouldReturnTrueToFindLongestLCS_LCSEqualOneOfString(){
+        LongestCommonSubsequence t = new LongestCommonSubsequence("abcd", "abc");
+        String expected = "abc";
+        String actual = t.findLCS();
+        assertEquals(expected, actual); 
+    }
+
+    @Test
+    public void shouldReturnTrueToFindLongestLCS_FirstTest(){
+        LongestCommonSubsequence t = new LongestCommonSubsequence("asftjsyjseadrhsthge", "abcde");
+        String expected = "ade";
+        String actual = t.findLCS();
+        assertEquals(expected, actual); 
+    }
+    
+    @Test
+    public void shouldReturnTrueToFindLongestLCS_SecondTest(){
+        LongestCommonSubsequence t = new LongestCommonSubsequence("piłem", "piłowałem");
+        String expected = "piłem";
+        String actual = t.findLCS();
+        assertEquals(expected, actual); 
+    }
+
+    @Test
+    public void shouldReturnTrueToFindLongestLCS_ThirdTest(){
+        LongestCommonSubsequence t = new LongestCommonSubsequence("abraKadabra", "AlaMaKota");
+        String expected = "aaKa";
+        String actual = t.findLCS();
+        assertEquals(expected, actual); 
+    }
+
+    @Test
+    public void shouldReturnTrue_LettersWithSlash(){
+        LongestCommonSubsequence t = new LongestCommonSubsequence("\t\b\n\b", "tbnb");
+        String expected = "";
+        String actual = t.findLCS();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnTrue_DisplayTestWithSlash(){
+        LongestCommonSubsequence t = new LongestCommonSubsequence("\b\t\n\f", "\'\"\\\r");
+        t.display();
+        String expected =
+        "+-------+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |\n" +
+        "|       |     | \\b  | \\t  | \\n  | \\f  |\n" +
+        "|       |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |\n" +
+        "|       |  0  |  0  |  0  |  0  |  0  |\n" +
+        "|       |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |\n" +
+        "|  \\\'   |  0  |  0  |  0  |  0  |  0  |\n" +
+        "|       |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |\n" +
+        "|  \\\"   |  0  |  0  |  0  |  0  |  0  |\n" +
+        "|       |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |\n" +
+        "|  \\\\   |  0  |  0  |  0  |  0  |  0  |\n" +
+        "|       |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |\n" +
+        "|  \\r   |  0  |  0  |  0  |  0  |  0  |\n" +
+        "|       |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+\n";
+        assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    public void shouldReturnTrue_DisplayTestWithEmptyStrings(){
+        LongestCommonSubsequence t = new LongestCommonSubsequence("", "");
+        t.display();
+        String expected =
+        "+-------+-----+\n" +
+        "|       |     |\n" +
+        "|       |     |\n" + 
+        "|       |     |\n" +
+        "+-------+-----+\n" + 
+        "|       |     |\n" +
+        "|       |  0  |\n" +
+        "|       |     |\n" +
+        "+-------+-----+\n";
+        assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    public void shouldReturnTrue_DisplayTestWithSimilarStrings(){
+        LongestCommonSubsequence t = new LongestCommonSubsequence("absd", "abs");
+        t.display();
+        String expected =
+        "+-------+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |\n" + 
+        "|       |     |  a  |  b  |  s  |  d  |\n" +
+        "|       |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |\n" +
+        "|       |  0  |  0  |  0  |  0  |  0  |\n" +
+        "|       |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+\n" +
+        "|       |     |\\    |     |     |     |\n" +
+        "|   a   |  0  |  1  |  1  |  1  |  1  |\n" +
+        "|       |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |\\    |     |     |\n" +
+        "|   b   |  0  |  1  |  2  |  2  |  2  |\n" +
+        "|       |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |\\    |     |\n" +
+        "|   s   |  0  |  1  |  2  |  3  |< 3  |\n" +
+        "|       |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+\n";
+        assertEquals(expected, outContent.toString());
+    }
+
+    @Test
+    public void shouldReturnTrue_DisplayTestWithDiferentStrings(){
+        LongestCommonSubsequence t = new LongestCommonSubsequence("HarryPotter", "Okulary");
+        t.display();
+        String expected = 
+        "+-------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "|       |     |  H  |  a  |  r  |  r  |  y  |  P  |  o  |  t  |  t  |  e  |  r  |\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "|       |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "|   O   |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "|   k   |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "|   u   |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "|   l   |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |  0  |\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |\\    |     |     |     |     |     |     |     |     |     |\n" +
+        "|   a   |  0  |  0  |  1  |< 1  |  1  |  1  |  1  |  1  |  1  |  1  |  1  |  1  |\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |\\    |     |     |     |     |     |     |     |\n" +
+        "|   r   |  0  |  0  |  1  |  2  |  2  |  2  |  2  |  2  |  2  |  2  |  2  |  2  |\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n" +
+        "|       |     |     |     |     |     |\\    |     |     |     |     |     |     |\n" +
+        "|   y   |  0  |  0  |  1  |  2  |  2  |  3  |< 3  |< 3  |< 3  |< 3  |< 3  |< 3  |\n" +
+        "|       |     |     |     |     |     |     |     |     |     |     |     |     |\n" +
+        "+-------+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n";
+        assertEquals(expected, outContent.toString());
     }
 }
